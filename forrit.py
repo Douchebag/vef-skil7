613642db4cb930533fb5d5875932608ba7eb94b8
Douchebag <yesimdingo@gmail.com>
2018-03-21 09:54:27 +0000

    login naestum done

--- forrit.py
+++ forrit.py
@@ -1,2 +1,58 @@
-from bottle import run, route, template, request, response
+from bottle import run, route, template, request, response, redirect, static_file, error
 
+'''
+@route('/')
+def index():
+    if request.get_cookie("hello"):
+        return "hello again"
+    else:
+        response.set_cookie("hello", "world")
+        return "hello world"
+'''
+
+adminuser = 'admin'
+adminpass = '12345'
+
+@route('/')
+def index():
+    return template('index.tpl')
+
+@route('/login')
+def login():
+    return template('login.tpl')
+
+@route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+
+    if username == adminuser and password == adminpass:
+        response.set_cookie("user", username, secret='AWDWd')
+        return redirect('/restricted')
+    else:
+        return "Login failed. <br> <a href='/login'>Tilbaka</a>"
+
+@route('/restricted')
+def restricted():
+    user = request.get_cookie("user", secret='AWDWd')
+    #if user == adminuser:
+    if user:
+        return "Restricted area <br> <a href='/logout'>Log out</a>"
+    else:
+        return "Aðgangur bannaður"
+
+@route('/logout')
+def logout():
+    response.set_cookie('user', "", expires=0)
+    return "Þú hefur verið skráður út. <br> <a href='/login'>Login</a>"
+
+@route("/static/<filename>")
+def server_static(filename):
+    return static_file(filename, root='./files')
+
+@error(404)
+def error404(error):
+    return "<h1>Þessi síða fannst ekki</h1><br><a href='/'>Heim</a>"
+
+#https://codepen.io/Lewitje/pen/BNNJjo
+run()
---
+++ views/login.tpl
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="is">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" href="/static/stylesheet.css" rel="stylesheet">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="/static/logform.js"></script>
+    <title>Innskráning</title>
+</head>
+<body>
+<div class="wrapper">
+	<div class="container">
+		<h1>Innskráning</h1>
+        <form action="/login" method="post">
+            <input name="username" type="text"><br>
+            <input name="password" type="password"><br>
+            <input value="Login" type="submit" /><br>
+        </form>
+    </div>
+
+    <ul class="bg-bubbles">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+	</ul>
+</div>
+</body>
+</html>
